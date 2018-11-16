9d0aa29cfc4038757e2c63e46095bdda5f4af280
Adam Zhou <adzhou218@gmail.com>
2018-11-16 13:17:38 -0500

    throwaway commit

---
+++ client/components/game-room.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+export class GameRoom extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <div>Hello</div>
+  }
+}
