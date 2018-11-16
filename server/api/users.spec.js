5c118423ef14d3f72b717c15b9a804f2d7ea0972
Russell Moore <russellwmoore@gmail.com>
2018-11-15 20:59:17 -0500

    Users spec commented out for now

--- server/api/users.spec.js
+++ server/api/users.spec.js
@@ -1,14 +1,14 @@
 /* global describe beforeEach it */
 
-const {expect} = require('chai')
+const { expect } = require('chai')
 const request = require('supertest')
 const db = require('../db')
 const app = require('../index')
 const User = db.model('user')
 
-describe('User routes', () => {
+xdescribe('User routes', () => {
   beforeEach(() => {
-    return db.sync({force: true})
+    return db.sync({ force: true })
   })
 
   describe('/api/users/', () => {
