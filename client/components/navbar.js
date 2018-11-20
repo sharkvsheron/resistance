71e4ed9a28d30ceb9e2298015faac962a06bfd78
Russell Moore <russellwmoore@gmail.com>
2018-11-20 16:14:43 -0500

    Add username to navbar

--- client/components/navbar.js
+++ client/components/navbar.js
@@ -4,7 +4,7 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { logout } from '../store'
 
-const Navbar = ({ handleClick, isLoggedIn }) => (
+const Navbar = ({ handleClick, isLoggedIn, user }) => (
   <div>
     <h1>THE RESISTANCE</h1>
     <nav>
@@ -15,6 +15,7 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
           <a href="#" onClick={handleClick}>
             Logout
           </a>
+          Welcome {user.userName}!
         </div>
       ) : (
           <div>
@@ -33,7 +34,8 @@ const Navbar = ({ handleClick, isLoggedIn }) => (
  */
 const mapState = state => {
   return {
-    isLoggedIn: !!state.user.id
+    isLoggedIn: !!state.user.id,
+    user: state.user
   }
 }
 
