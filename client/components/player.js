12c75fb065c5f403238b41c4264283cab2bc010e
Adam Zhou <adzhou218@gmail.com>
2018-11-21 18:05:04 -0500

    comitting changes before deploy attempt

--- client/components/player.js
+++ client/components/player.js
@@ -7,12 +7,11 @@ export default class Player extends Component {
     super(props)
   }
   render() {
-    const {userName, roleId} = this.props.player
+    const {userName, roleId, sessionKey} = this.props.player
     const {id} = this.props
     return (
       <div className="player-card" id={`player${id}`}>
         <div className="videoWrapper" id={`role${roleId}`} />
-
         <h3>username: {userName}</h3>
         <h3>role id: {roleId}</h3>
         <button type="submit">WAITING FOR PLAYER TO JOIN</button>
