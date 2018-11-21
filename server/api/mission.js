904ba0e75a4d759e27d2f9ea389d16213eb3c027
Russell Moore <russellwmoore@gmail.com>
2018-11-21 12:56:14 -0500

    Add nominator submits a nomiation function

--- server/api/mission.js
+++ server/api/mission.js
@@ -1,19 +1,26 @@
 const router = require('express').Router()
 const Sequelize = require('sequelize')
 const Op = Sequelize.Op
-const {Game, User, GameType, Nomination} = require('../db/models')
+const { Game, User, GameType, Nomination } = require('../db/models')
+const { submitNomination } = require('../socket/functions')
 module.exports = router
+
+router.get('/test/:userId', async (req, res, next) => {
+  const response = await submitNomination(req.params.userId, [1, 2])
+  res.json(response)
+})
+
 
 router.put('/:userId', async (req, res, next) => {
   try {
     // const {userId} = req.session.userId
-    const {userId} = req.params
-    const {missionResult} = req.body
+    const { userId } = req.params
+    const { missionResult } = req.body
     const user = await User.findById(userId)
     const game = await Game.findById(user.gameId)
-    const nomination = await Nomination.findOne({where: {id: userId}})
+    const nomination = await Nomination.findOne({ where: { id: userId } })
     if (Number(userId) === nomination.dataValues.userId) {
-      await nomination.update({missionStatus: missionResult})
+      await nomination.update({ missionStatus: missionResult })
     } else {
       res.send('Unathorized')
     }
