8b6658970ffb64c9e46146929b9749b2b215c3a8
peter giampaoli <petergiampaoli@gmail.com>
2018-11-26 09:19:17 -0500

    replaced images

---
+++ client/components/game.js
@@ -0,0 +1,31 @@
+import React, {Component} from 'react'
+import {Link} from 'react-router-dom'
+import {Card, CardTitle} from 'react-materialize'
+
+export default class Game extends Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return (
+      <div className="game-card">
+        <Card
+          style={{'max-height': '82%'}}
+          className="small"
+          header={
+            <CardTitle image="images/space-background.jpg">
+              GAME ROOM # {this.props.game.id}
+            </CardTitle>
+          }
+          actions={[
+            <Link to={`/game/${this.props.game.id}`} key={this.props.game.id}>
+              Click to enter game #{this.props.game.id}
+            </Link>
+          ]}
+        >
+          {' '}
+        </Card>
+      </div>
+    )
+  }
+}
